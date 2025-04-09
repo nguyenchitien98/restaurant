2ffb10f9f9dc342256c6558928a935e1b65f281b
nguyenchitien98 <tiennguyenchi98@gmail.com>
2025-04-09 14:19:20 +0700

    Create front end react app for restaurant app

---
+++ FrontEnd-React-DashBoard/tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  darkMode: 'class', // 👈 Bật dark mode theo class
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
