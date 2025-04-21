dbfcfca8096a24e43ea765ae8a7a36605aa043b8
nguyenchitien98 <tiennguyenchi98@gmail.com>
2025-04-21 15:33:59 +0700

    Update code swagger for API gateway

--- Gateway-Service/src/main/java/com/example/filters/GatewayConfig.java
+++ Gateway-Service/src/main/java/com/example/filters/GatewayConfig.java
@@ -6,6 +6,7 @@ import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+//cấu hình các route thủ công qua Java DSL
 @Configuration
 public class GatewayConfig {
     @Bean
